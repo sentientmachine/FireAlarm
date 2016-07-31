78a11386b5948c908e4550e0e92d7b3b2ce1746e
Ashish <shivashish.ahuja@gmail.com>
2016-07-31 11:59:25 +0530

    Code so that a user can get notified for a specific tag

--- chatbot/misc_commands.h
+++ chatbot/misc_commands.h
@@ -14,7 +14,7 @@
 
 void listCommands(RunningCommand *command, void *ctx) {
     ChatBot *bot = ctx;
-    
+
     postReply(bot->room, "Running commands:", command->message);
     const size_t maxLineSize = 256;
     pthread_mutex_lock(&bot->runningCommandsLock);
@@ -28,12 +28,12 @@ void listCommands(RunningCommand *command, void *ctx) {
     for (int i = 0; i < bot->runningCommandCount; i++) {
         const size_t maxUsernameLength = 16;
         const size_t maxCommandLength = 48;
-        
+
         char username[strlen(bot->runningCommands[i]->message->user->name) + 1];
         strcpy(username, bot->runningCommands[i]->message->user->name);
         char message[strlen(bot->runningCommands[i]->message->content) + 1];
         strcpy(message, bot->runningCommands[i]->message->content);
-        
+
         //max username length = 16 chars
         if (strlen(username) > maxUsernameLength) {
             username[maxUsernameLength-3] = '.';
@@ -48,15 +48,15 @@ void listCommands(RunningCommand *command, void *ctx) {
             message[maxCommandLength-1] = '.';
             message[maxCommandLength] = '\0';
         }
-        
+
         snprintf(messageString + strlen(messageString), maxLineSize,
                  "    %*s%*s|%*s%*s\n",
                  (int)((maxUsernameLength/2)+strlen(username)/2),
                  username,
                  (int)((maxUsernameLength/2)-strlen(username)/2),
                  "",
-                 
-                 
+
+
                  (int)((maxCommandLength/2)+strlen(message)/2),
                  message,
                  (int)((maxCommandLength/2)-strlen(message)/2),
@@ -71,15 +71,15 @@ void listCommands(RunningCommand *command, void *ctx) {
 void help (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     postReply (bot->room, "I'm  [Fire Alarm](https://github.com/NobodyNada/chatbot), a bot which detects questions that need closing whenever they are posted or edited. [My command list is available here.](https://github.com/NobodyNada/chatbot/wiki/Commands)", command->message);
     return;
 }
 
-void aliveCheck (RunningCommand *command, void *ctx) 
+void aliveCheck (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     postReply (bot->room, "Why did you think I was dead?", command->message);
     return;
 }
@@ -87,7 +87,7 @@ void aliveCheck (RunningCommand *command, void *ctx)
 void commandList (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     postReply (bot->room, "To see a complete list of commands, visit [this page](https://github.com/NobodyNada/chatbot/wiki/Commands).", command->message);
     return;
 }
@@ -95,9 +95,9 @@ void commandList (RunningCommand *command, void *ctx)
 void changeThreshold (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     long newThreshold = strtol (command->argv [0], NULL, 10);
-    
+
     if (newThreshold > 10000)
     {
         postReply (bot->room, "Please enter a threshold smaller than 10000.", command->message);
@@ -108,7 +108,7 @@ void changeThreshold (RunningCommand *command, void *ctx)
         postReply (bot->room, "Please enter a threshold bigger than 100.", command->message);
         return;
     }
-    
+
     THRESHOLD = newThreshold;
     return;
 }
@@ -116,11 +116,11 @@ void changeThreshold (RunningCommand *command, void *ctx)
 void checkThreshold (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     char message [256];
-    
+
     sprintf (message, "The current threshold is %ld.", THRESHOLD);
-    
+
     postReply (bot->room, message, command->message);
     return;
 }
@@ -128,69 +128,276 @@ void checkThreshold (RunningCommand *command, void *ctx)
 void optIn (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-     
+    int check = 0;
+    char *arg;
+    long userID = command->message->user->userID;
+
+    if (command->argc > 0)
+    {
+        check = 1;
+        arg = command->argv [0];
+        removeChar (arg, '[');
+        removeChar (arg, ']');
+        if (arg [0] == 't' && arg [1] == 'a' && arg [2] == 'g' && arg [3] == ':')
+        {
+            arg += 4; // Chopping of the first 4 characters
+        }
+    }
+
+    Notify *n = getNotificationByID (bot, userID);
+
+    if (!check)
+    {
+        arg = NULL;
+        if (n != NULL)
+        {
+            postReply (bot->room, "You are already in the notification list.", command->message);
+            return;
+        }
+    }
+
+    if (!isValidTag (bot, arg))
+    {
+        postReply (bot->room, "Please enter a valid tag.", command->message);
+        return;
+    }
+
+    if (check)
+    {
+        if (isTagInNotification(n, arg))
+        {
+            char *message;
+            asprintf (&message, "You are already opted in for [tag:%s].", arg);
+            postReply (bot->room, message, command->message);
+            free (message);
+            return;
+        }
+    }
+
+    if (n != NULL && check)
+    {
+        addTagNotification (n, arg);
+        char *message;
+        asprintf (&message, "Added [tag:%s] to your current notification.", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
     Notify **notify = bot->notify = realloc(bot->notify, ++bot->totalNotifications * sizeof(Notify*));
-    
-    notify[bot->totalNotifications - 1] = createNotification (0, command->message->user->userID);
-    
+
+    notify[bot->totalNotifications - 1] = createNotification (0, command->message->user->userID, arg, -1);
+
     postReply (bot->room, "Opt-in successful. You will now be pinged for reports.", command->message);
-    
+
     return;
 }
 
 void notifyMe (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+    int check = 0;
+    char *arg;
+    long userID = command->message->user->userID;
+
+    if (command->argc > 0)
+    {
+        check = 1;
+        arg = command->argv [0];
+        removeChar (arg, '[');
+        removeChar (arg, ']');
+        if (arg [0] == 't' && arg [1] == 'a' && arg [2] == 'g' && arg [3] == ':')
+        {
+            arg += 4; // Chopping of the first 4 characters
+        }
+    }
+
+    Notify *n = getNotificationByID (bot, userID);
+
+    if (!check)
+    {
+        arg = NULL;
+        if (n != NULL && n->type == 1)
+        {
+            postReply (bot->room, "You are already in the notification list.", command->message);
+            return;
+        }
+    }
+
+    if (!isValidTag (bot, arg))
+    {
+        postReply (bot->room, "Please enter a valid tag.", command->message);
+        return;
+    }
+
+    if (check)
+    {
+        if (isTagInNotification(n, arg))
+        {
+            char *message;
+            asprintf (&message, "You are already opted in for [tag:%s].", arg);
+            postReply (bot->room, message, command->message);
+            free (message);
+            return;
+        }
+    }
+
+    if (n != NULL && check)
+    {
+        addTagNotification (n, arg);
+        char *message;
+        asprintf (&message, "Added [tag:%s] to your current notification.", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
     Notify **notify = bot->notify = realloc(bot->notify, ++bot->totalNotifications * sizeof(Notify*));
-    
-    notify[bot->totalNotifications - 1] = createNotification (1, command->message->user->userID);
+
+    notify[bot->totalNotifications - 1] = createNotification (1, command->message->user->userID, arg, -1);
     bot->totalNotifications ++;
-    
+
     postReply (bot->room, "Notify submission successful. You will now be notified for reports.", command->message);
-    
+
     return;
 }
 
 void optOut (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+    int check = 0;
+    char *arg = NULL;
+
+    if (command->argc > 0)
+    {
+        check = 1;
+        arg = command->argv [0];
+        removeChar (arg, '[');
+        removeChar (arg, ']');
+        if (arg [0] == 't' && arg [1] == 'a' && arg [2] == 'g' && arg [3] == ':')
+        {
+            arg += 4; // Chopping of the first 4 characters
+        }
+    }
+
     long userID = command->message->user->userID;
-    
+
     Notify *notify = getNotificationByID (bot, userID);
-    
-    if (notify == NULL || notify->type == 1)
+
+    if (notify == NULL)
     {
         postReply (bot->room, "You are already opted out.", command->message);
         return;
     }
-    
+
+    int isUserOptedTag = 0;
+    for (int i = 0; i < totalTags; i ++)
+    {
+        if (strcmp (arg, notify->tags [i]) == 0)
+        {
+            isUserOptedTag = 1;
+        }
+    }
+
+    if (!isUserOptedTag)
+    {
+        char *message;
+        asprintf (&message, "You are already not opted in for [tag:%s].", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
+    if (check && notify->totalTags == 1)
+    {
+        deleteNotification (bot, notify);
+        postReply (bot->room, "Opt-out successful. You will now not be pinged for reports.", command->message);
+        return;
+    }
+    else if (check)
+    {
+        deleteTagNotification (notify, arg);
+        char *message;
+        asprintf (&message, "Opt-out successful. You will now not be pinged for reports with [tag:%s].", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
     deleteNotification (bot, notify);
-    
+
     postReply (bot->room, "Opt-out successful. You will now not be pinged for reports.", command->message);
-    
+
     return;
 }
 
 void unnotifyMe (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
-    long userID = command->message->user->userID;
-    
-    Notify *notify = getNotificationByID (bot, userID);
-    
-    if (notify == NULL || notify->type == 0)
+    int check = 0;
+    char *arg = NULL;
+
+    if (command->argc > 0)
     {
-        postReply (bot->room, "You are already not notified.", command->message);
+        check = 1;
+        arg = command->argv [0];
+        removeChar (arg, '[');
+        removeChar (arg, ']');
+        if (arg [0] == 't' && arg [1] == 'a' && arg [2] == 'g' && arg [3] == ':')
+        {
+            arg += 4; // Chopping of the first 4 characters
+        }
+    }
+
+    long userID = command->message->user->userID;
+
+    Notify *notify = getNotificationByID (bot, userID);
+
+    if (notify == NULL || notify->type == 1)
+    {
+        postReply (bot->room, "You are already not in the notification list.", command->message);
         return;
     }
-    
+
+    int isUserOptedTag = 0;
+    for (int i = 0; i < totalTags; i ++)
+    {
+        if (strcmp (arg, notify->tags [i]) == 0)
+        {
+            isUserOptedTag = 1;
+        }
+    }
+
+    if (!isUserOptedTag)
+    {
+        char *message;
+        asprintf (&message, "You are already not opted in for [tag:%s].", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
+    if (check && notify->totalTags == 1)
+    {
+        deleteNotification (bot, notify);
+        postReply (bot->room, "Opt-out successful. You will now not be pinged for reports.", command->message);
+        return;
+    }
+    else if (check)
+    {
+        deleteTagNotification (notify, arg);
+        char *message;
+        asprintf (&message, "Opt-out successful. You will now not be pinged for reports with [tag:%s].", arg);
+        postReply (bot->room, message, command->message);
+        free (message);
+        return;
+    }
+
+
     deleteNotification (bot, notify);
-    
+
     postReply (bot->room, "You will now not be notified for any reports.", command->message);
-    
+
     return;
 }
 
@@ -199,7 +406,7 @@ void say(RunningCommand *command, void *ctx) {
     size_t messageLength = 1;
     char *message = malloc(messageLength);
     *message = 0;
-    
+
     for (int i = 0; i < command->argc; i++) {
         messageLength += strlen(command->argv[i]) + 2;
         message = realloc(message, messageLength);
@@ -213,10 +420,10 @@ void amINotified (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
     long userID = command->message->user->userID;
-    
+
     Notify *notify = getNotificationByID (bot, userID);
-    
-    if (notify == NULL)
+
+    /*if (notify == NULL)
     {
         postReply (bot->room, "You are not currently in the notification list.", command->message);
     }
@@ -227,37 +434,96 @@ void amINotified (RunningCommand *command, void *ctx)
     else if (notify->type == 1)
     {
         postReply (bot->room, "You are currently notified of all reports.", command->message);
+    }*/
+
+    int i;
+    if (notify == NULL)
+    {
+        postReply (bot->room, "You are currently not in the notification list.", command->message);
+        return;
     }
-    
-    return;
+    else if (notify->type == 0)
+    {
+        if (notify->totaltags == -1)
+        {
+            postReply (bot->room, "You are currently opted in.", command->message);
+            return;
+        }
+        else if (notify->totalTags > 0)
+        {
+            char *message = malloc (sizeof (char) * 127);
+            sprintf (message, "You are currently opted in for the %s ", notify->totalTags ? "tag" : "tags");
+            for (i = 0; i < notify->totalTags; i ++)
+            {
+                if (i == notify->totalTags - 1)
+                {
+                    sprintf (message + strlen (message), "[tag:%s].", notify->tags [i]);
+                }
+                else
+                {
+                    sprintf (message + strlen (message), "[tag:%s], ", notify->tags [i]);
+                }
+            }
+            postReply (bot->room, message, comand->message);
+            free (message);
+            return;
+        }
+    }
+    else if (notify->type == 1)
+    {
+        if (notify->totalTags == -1)
+        {
+            postReply (bot->room, "You are currently notified of all reports.", command->message);
+            return;
+        }
+        else if (notify->totalTags > 0)
+        {
+            char *message = malloc (sizeof (char) * 127);
+            sprintf (message, "You are currently notified of report with the %s ", notify->totalTags ? "tag" : "tags");
+            for (i = 0; i < notify->totalTags; i ++)
+            {
+                if (i == notify->totalTags -1)
+                {
+                    sprintf (message + strlen (message), "[tag:%s].", notify->tags [i]);
+                }
+                else
+                {
+                    sprintf (message + strlen (message), "[tag:%s], ", notify->tags [i]);
+                }
+            }
+            postReply (bot->room, message, command->message);
+            free (message);
+            return;
+        }
+    }
 }
 
 void isNotified (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     if (command->argc == 0)
     {
         postReply (bot->room, "One argument required.", command->message);
         return;
     }
-    
+
     long userID = strtol (command->argv [0], NULL, 10);
-    
+
     if (!isValidUserID (bot, userID))
     {
         postReply (bot->room, "Please enter a valid user id.", command->message);
         return;
     }
-    
+
     if (command->message->user->userID == userID)
     {
         amINotified (command, ctx);
         return;
     }
-    
+
     Notify *notify = getNotificationByID (bot, userID);
-    
+
     if (notify == NULL)
     {
         postReply (bot->room, "That user is no currently in the notification list.", command->message);
@@ -270,87 +536,87 @@ void isNotified (RunningCommand *command, void *ctx)
     {
         postReply (bot->room, "That user is currently notified for all reports.", command->message);
     }
-    
+
     return;
 }
 
 void printNotifiedUsers (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     char *message = malloc (sizeof (127));
     char *messageString = malloc (sizeof (127 * (bot->totalNotifications + 2)));
-    
+
     snprintf (message, 127, "There are %d total notified users: ", bot->totalNotifications);
     postReply (bot->room, message, command->message);
-    
+
     Notify **notifications = bot->notify;
-    
+
     sprintf (messageString, "Opted-in:\n");
-    
+
     for (int i = 0; i < bot->totalNotifications; i ++)
     {
         Notify *notify = notifications [i];
-        
+
         if (notify->type == 0)
         {
             sprintf (messageString + strlen (messageString), "%s\n", getUsernameByID (bot, notify->userID));
         }
     }
-    
+
     sprintf (messageString + strlen (messageString), "\nNotified Users:\n");
-    
+
     for (int i = 0; i < bot->totalNotifications; i ++)
     {
         Notify *notify = notifications [i];
-        
+
         if (notify->type == 1)
         {
             sprintf (messageString + strlen (messageString), "%s\n", getUsernameByID (bot, notify->userID));
         }
     }
-    
+
     postMessage (bot->room, messageString);
-    
+
     free (messageString);
     free (message);
-    
+
     return;
 }
 
 void apiQuota (RunningCommand *command, void *ctx)
 {
     ChatBot *bot = ctx;
-    
+
     pthread_mutex_lock(&bot->room->clientLock);
     CURL *curl = bot->room->client->curl;
-    
+
     checkCURL(curl_easy_setopt(curl, CURLOPT_HTTPGET, 1));
     OutBuffer buffer;
     buffer.data = NULL;
     checkCURL(curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer));
-    
+
     curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip, deflate");
-    
+
     unsigned max = 256;
     char request [max];
-    
+
     snprintf (request, max,
               "api.stackexchange.com/2.2/info?site=stackoverflow");
-              
+
     curl_easy_setopt(curl, CURLOPT_URL, request);
-    
+
     checkCURL(curl_easy_perform(curl));
-    
+
     checkCURL(curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, ""));
-    
-    
+
+
     pthread_mutex_unlock(&bot->room->clientLock);
-    
+
     cJSON *json = cJSON_Parse(buffer.data);
-    
+
     free(buffer.data);
-    
+
     if (!json || cJSON_GetObjectItem(json, "error_id")) {
         if (json) {
             cJSON_Delete(json);
@@ -358,7 +624,7 @@ void apiQuota (RunningCommand *command, void *ctx)
         puts("Error fetching post!");
         return 0;
     }
-    
+
     cJSON *backoff;
     if ((backoff = cJSON_GetObjectItem(json, "backoff"))) {
         char *str;
@@ -366,16 +632,16 @@ void apiQuota (RunningCommand *command, void *ctx)
         postMessage(bot->room, str);
         free(str);
     }
-    
+
     int apiQuota = cJSON_GetObjectItem (json, "quota_remaining")->valueint;
-    
+
     cJSON_Delete (json);
-    
+
     char *message;
-    
+
     asprintf (&message, "The current api quota is %d.", apiQuota);
     postReply (bot->room, message, command->message);
-    
+
     free (message);
     return;
 }
